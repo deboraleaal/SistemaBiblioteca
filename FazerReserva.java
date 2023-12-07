17a37df225c249a2314ab89c39d0710b0f5a50be
deboraleaal <37719173+deboraleaal@users.noreply.github.com>
2023-12-06 23:56:29 -0300

    Update FazerReserva.java

--- FazerReserva.java
+++ FazerReserva.java
@@ -1,7 +1,31 @@
-public class FazerReserva {
-    public boolean fazerReserva(Usuario usuario, Livro livro) {
-        // L�gica para reserva
-        // ...
-        return true; // ou false em caso de insucesso
-    }
-}
+import java.util.ArrayList;
+import java.util.Date;
+
+public class FazerReserva extends Usuario {
+    
+		public FazerReserva(String nome, int codigo) {
+		super(nome, codigo);
+		// TODO Auto-generated constructor stub
+	}
+
+		public ArrayList<Reserva> getReservasUsuario() {
+    		return ReservasUsuario;
+    	}
+
+    	public void reservarLivro(Livro livroReserva){
+    		int contadorDeReservas = 0;
+    		for (int i = 0; i < ReservasUsuario.size(); i++) {
+                if (ReservasUsuario.get(i).dataReserva == new Date()) {
+                    contadorDeReservas += 1;
+                }
+            }
+//Precisa dar uma olhada nessa parte que ainda está apresentando erro
+
+    		if(contadorDeReservas < 3){
+    		  ReservasUsuario.add(livroReserva.tentarReserva(codigo));
+    		}else{
+    			System.out.println("Não foi possível efetuar reserva");
+    		}
+    	}
+}  
+    
