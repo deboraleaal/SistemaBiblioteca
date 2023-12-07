7403fdd0b576471332deece744796be5448c6552
deboraleaal <37719173+deboraleaal@users.noreply.github.com>
2023-12-06 21:58:52 -0300

    Update Reserva.java

--- Reserva.java
+++ Reserva.java
@@ -1,4 +1,13 @@
-
+import java.util.Date;
 public class Reserva {
+    
+   public Livro livroReservado;
+   public Date dataReserva;
+   public int codUsuarioReserva;
 
+   public Reserva(Livro livro, Date data, int codUsuario){
+    livroReservado = livro;
+    dataReserva = data;
+    codUsuarioReserva = codUsuario;
+  }
 }
