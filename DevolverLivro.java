407c6b124199fa9d6372d508cebfbf3c79b8a837
Cleyton Solares <99514053+Cleyton22@users.noreply.github.com>
2023-12-07 14:16:52 -0300

    Delete DevolverLivro.java

--- DevolverLivro.java
+++
@@ -1,7 +0,0 @@
-public class DevolverLivro {
-    public boolean devolverLivro(Usuario usuario, Livro livro) {
-        // Lógica para devolução
-        // ...
-        return true; // ou false em caso de insucesso
-    }
-}
