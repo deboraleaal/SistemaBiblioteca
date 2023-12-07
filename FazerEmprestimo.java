7108fce92fb736d9631edd93bf3983da2bf69034
Cleyton Solares <99514053+Cleyton22@users.noreply.github.com>
2023-12-07 14:16:19 -0300

    Delete FazerEmprestimo.java

--- FazerEmprestimo.java
+++
@@ -1,7 +0,0 @@
-public class FazerEmprestimo {
-    public boolean fazerEmprestimo(Usuario usuario, Livro livro) {
-        // Lógica para empréstimo
-        // ...
-        return true; // ou false em caso de insucesso
-    }
-}
