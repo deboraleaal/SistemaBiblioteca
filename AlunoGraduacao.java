a926064581da781f9add483fa604159479c5d125
Cleyton Solares <99514053+Cleyton22@users.noreply.github.com>
2023-12-10 00:34:36 -0300

    Add files via upload

--- AlunoGraduacao.java
+++ AlunoGraduacao.java
@@ -1,7 +1,7 @@
 package sistemaBibliotecaESoficial;
 
 public class AlunoGraduacao extends Usuario {
-	public AlunoGraduacao(int codigo, String nome, int tempoEmprestimo, IVerificadorEmprestimo verificador) {
+	public AlunoGraduacao(String codigo, String nome, int tempoEmprestimo, IVerificadorEmprestimo verificador) {
 		super(codigo, nome, tempoEmprestimo, verificador);
 		this.setLimiteEmprestimo(3);
 	}
