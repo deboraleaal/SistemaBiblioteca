50df96bea3e760a3d3cc7a66f62995dac571868e
Cleyton Solares <99514053+Cleyton22@users.noreply.github.com>
2023-12-10 00:35:09 -0300

    Delete AlunoGraduacao.java

--- AlunoGraduacao.java
+++
@@ -1,9 +0,0 @@
-package sistemaBibliotecaESoficial;
-
-public class AlunoGraduacao extends Usuario {
-	public AlunoGraduacao(String codigo, String nome, int tempoEmprestimo, IVerificadorEmprestimo verificador) {
-		super(codigo, nome, tempoEmprestimo, verificador);
-		this.setLimiteEmprestimo(3);
-	}
-
-}
