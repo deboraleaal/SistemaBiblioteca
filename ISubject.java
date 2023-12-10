480e62aa390694c56d08b82e4e83d966d1118ff0
Cleyton Solares <99514053+Cleyton22@users.noreply.github.com>
2023-12-10 00:37:33 -0300

    Delete ISubject.java

--- ISubject.java
+++
@@ -1,7 +0,0 @@
-package sistemaBibliotecaESoficial;
-
-public interface ISubject {
-	public void registraObservador(IObservador observador);
-	public void removerObservador(IObservador observador);
-	public void notificarObservadores();
-}
